47a3002ba4eb7c3fb42a969fa1eb5fbe3625a6c9
parthg19 <parthg54321@gmail.com>
2022-01-16 00:59:19 +0530

    hbs

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ const makeCommit = n => {
     const x = random.int(0, 54);
     const y = random.int(0, 6);
 
-    const DATE = moment().subtract(1,'d').add(1, 'd').add(x, 'w').add(y, 'd').format();
+    const DATE = moment().subtract(1,'y').add(1, 'd').add(x, 'w').add(y, 'd').format();
     const data ={
         date: DATE
     }
-
+    console.log(DATE);
     jsonfile.writeFile(FILE_PATH, data, ()=>{
         simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE},
         makeCommit.bind(this, --n));
